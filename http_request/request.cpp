f909794a219679f2b765a840f298221f86f288d5
schaehun <schaehun@student.42.fr>
2023-11-29 23:37:01 +0100

    [UPDATE] parse/http_request/startline header body

--- http_request/request.cpp
+++ http_request/request.cpp
@@ -7,6 +7,11 @@
  * source: https://stackoverflow.com/questions/6686261/what-at-the-bare-minimum-is-required-for-an-http-request
 */
 
+/*
+ * About invalid HTTP request startline 
+ * https://stackoverflow.com/questions/7512823/c-insert-into-stdmap-without-knowing-a-key
+*/
+
 #include "request.hpp"
 
 int parse_http_request_startline(Request &request, std::string startline)
@@ -15,12 +20,23 @@ int parse_http_request_startline(Request &request, std::string startline)
 	std::istream_iterator<std::string> begin(ss);
 	std::istream_iterator<std::string> end;
 	std::vector<std::string> vstrings(begin, end);
-	if (vstrings.size() != 3)
-		return 1;
-	request.startline["method"] = vstrings[0];
-	request.startline["http_version"] = vstrings[2];
-	//if (!(*(std::end(vstrings) - 1)).compare("\r\n"))
-	//	return 1;
+	if (vstrings.size() < 1 || vstrings.size() > 3)
+		return 1; // still has to send a bad request = webserver is the one who decides about integrity
+	request.startline["method"] = "GET";
+	request.startline["uri"] = "/"; // don't know yet what is the default value
+	request.startline["http_version"] = "HTTP/1.0"; // by default
+	switch (vstrings.size())
+	{
+		case 3:
+			request.startline["http_version"] = vstrings[2];
+			// fall through
+		case 2:
+			request.startline["uri"] = vstrings[1]; // how to detect if it's 'http version' or 'uri'
+			// fall through
+		case 1:
+			request.startline["method"] = vstrings[0];
+			// fall through
+	}
 	return 0;
 }
 
@@ -33,25 +49,42 @@ int parse_http_request(Request &request, std::string req)
 	{
 		token = req.substr(last, next - last);
 		if (parse_http_request_startline(request, token) == 1)
-			std::cout << "syntax error 400" << std::endl;
+			return 400;
 		last = next + 2;
 	}
-	std::cout << request.startline["method"] << std::endl;
-	std::cout << request.startline["http_version"] << std::endl;
 	while ((next = req.find(delim, last)) != std::string::npos)
 	{
 		token = req.substr(last, next - last);
 		last = next + 2;
+		if (token == "") // double CRLF '\r\n\r\n' = the end of header
+			break ;
+		request.header.push_back(token); // split into key:value format on response side
 	}
-	//std::cout << req.substr(last);
+	request.body = req.substr(last, std::string::npos);
 	return 0;
+}
+
+void print_http_request(Request &request)
+{
+	std::cout << "=============================================\n";
+	std::cout << "Method : " << request.startline["method"] << std::endl;
+	std::cout << "Uri    : " << request.startline["uri"] << std::endl;
+	std::cout << "Version: " << request.startline["http_version"] << std::endl;
+	std::cout << "=============================================\n";
+	for (int i = 0; i < request.header.size(); i++)
+	{
+		std::cout << "Header : " << request.header[i] << std::endl;
+	}
+	std::cout << "=============================================\n";
+	std::cout << "Body   :\n" << request.body;
+	std::cout << "=============================================\n";
 }
 
 int main()
 {
 	Request request;
 
-	parse_http_request(request, "GET / HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n");
-	
+	parse_http_request(request, "GET / HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n<html>\r\n\t<body></body>\r\n</html>\r\n");
+	print_http_request(request);
 	return 0;
 }
--- http_request/request.hpp
+++ http_request/request.hpp
@@ -4,16 +4,22 @@
 # include <iostream>
 # include <cstring>
 # include <map>
+# include <vector>
 # include <bits/stdc++.h>
 
 class Request
 {
 	public:
 		std::map<std::string, std::string> startline;
-		std::map<std::string, std::string> header;
+		std::vector<std::string> header;
+		std::string body;
 	public:
-		Request(){};
-
+		Request(){
+			this->startline["method"] = "";
+			this->startline["uri"] = "";
+			this->startline["http_version"] = "";
+			this->body = "";
+		};
 };
 
 #endif
